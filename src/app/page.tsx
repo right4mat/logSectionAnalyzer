89204f0a9db49ec1ddf59d758e0dfd0d7a37f183
Luke Daniels <lukesdaniels92@gmail.com>
2025-05-03 20:46:49 +1000

    chanegs

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+import { LatestPost } from "~/app/_components/post";
+import { api, HydrateClient } from "~/trpc/server";
+import LogAnalyzer from "~/app/_components/LogAnalyzer";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+
+  void api.post.getLatest.prefetch();
+
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            Log Section{" "}
+            <span className="text-[hsl(280,100%,70%)]">Analyzer</span>
+          </h1>
+
+          <LogAnalyzer />
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
